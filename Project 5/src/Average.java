7bebb2a8f5806d8951ff6da2020f79a34f6e07d7
Spring <rspring@cslab18.cs.skidmore.edu>
2014-10-29 21:14:12 -0400

    Project 5 getting better

--- Project 5/src/Average.java
+++ Project 5/src/Average.java
@@ -1,11 +1,40 @@
+import java.util.*;
+import java.io.*;
 
 public class Average {
 	//read input, create array, perform analysis
+	double butterfly= 0.0;
+	double freestyle= 0.0;
 	SeasonTime [] times;
 	
 	//load method to determine size of array, numlines function
-public void load (String filepath){
-	this.times= new SeasonTime[]
+public void load (String filepath) throws FileNotFoundException{
+	this.times= new SeasonTime[this.numlines(filepath)];
+	Scanner fileReader = new Scanner(new File(filepath));
+	int i = 0;
+	
+
+	
+ while (fileReader.hasNext()){
+		/* Get the event */
+		String event = fileReader.next();
+		/* Get the time. */
+		double time = fileReader.nextDouble();
+	
+ 	
+ 
+	
+	
+	
+
+
+	/* Add a an object to the array with the values for event and time. */
+	times[i] = new SeasonTime(event,time);
+	i++;
+ }
+	fileReader.close();
+	
+	
 
 }
 /**
@@ -13,8 +42,9 @@ public void load (String filepath){
  * @param filepath
  * @see https://github.com/kevinfodness/cs106/blob/master/Project4b/src/Project4b.java
  * @return
+ * @throws FileNotFoundException 
  */
-public int numlines (String filepath){
+public int numlines (String filepath) throws FileNotFoundException{
 	int numLines = 0;
 	Scanner fileReader = new Scanner(new File(filepath));
 	while (fileReader.hasNextLine()) {
@@ -22,11 +52,29 @@ public int numlines (String filepath){
 	numLines++;
 	}
 	fileReader.close();
+	return numLines;
+}
+
+	public void analyze (){
+		
+		// declare variables to count the number of freestyle vs. butterfly entries
+		// start at 0
+		// increment in if statement
+		for (SeasonTime t : this.times){
 	
-}
-public double analyze (??){
-	//1. get total time for freestyle, then divide by total number of elements in freestyle (12)
-	//2. get total time for butterfly, then divide by total number of elements in butterfly (12)
-	//3. print out results of average for both to an output file
-}
+			if (t.event.equals("freestyle")){
+				freestyle += t.time;
+				
+			} else{
+				butterfly += t.time;
+			}			
+		}
+	
+		double avfreestyle= (freestyle/12);
+		double avbutterfly= (butterfly/12);
+		
+		System.out.println("Average Freestyle Time "+ avfreestyle+ " seconds");
+		System.out.println("Average Butterfly Time "+ avbutterfly+ " seconds");
+		
+	}
 }
--- Project 5/src/mainclass.java
+++ Project 5/src/mainclass.java
@@ -1,4 +1,11 @@
 
-public class mainclass {
+import java.io.*;
+import java.util.*;
 
+public class mainclass {
+	public static void main(String[] args) throws FileNotFoundException {
+		Average a = new Average();
+		a.load("src/swimtimes");
+		a.analyze();
+	}
 }
